1330fc37fe5118251f0b0c748895b1cdc19e29f7
Shomik <shomikchatterjee@gmail.com>
2023-12-18 01:42:19 -0600

    added project files initialized via start.spring.io

---
+++ src/main/java/bot/headtrawler/trawler/TrawlerApplication.java
@@ -0,0 +1,13 @@
+package bot.headtrawler.trawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bot/headtrawler/trawler/TrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package bot.headtrawler.trawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
